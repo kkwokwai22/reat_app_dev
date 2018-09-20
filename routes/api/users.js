4deae82c63b2f519ec62e6555e6efbd08c1d65d4
kkwokwai22 <kkwokwai22@gmail.com>
2018-09-20 13:58:40 -0700

    finish passport

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,10 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys").secretOrKey;
+const passport = require("passport");
+
+// Load Input Validation
+const validateRegisterInput = require("../../validation/register");
 
 // Load User model
 const User = require("../../models/User");
@@ -88,5 +92,20 @@ router.post("/login", (req, res) => {
     });
   });
 });
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
 
 module.exports = router;
