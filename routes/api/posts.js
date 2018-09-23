70cf742dc992e4d49e590a32b6144450efff6d89
kkwokwai22 <kkwokwai22@gmail.com>
2018-09-23 08:05:43 -0700

    finish up profile

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,6 +2,9 @@
 const express = require("express");
 const router = express.Router();
 
+// @route   GET api/posts/test
+// @desc    Tests post route
+// @access  Public
 router.get("/test", (req, res) =>
   res.json({
     msg: "Posts Works"
