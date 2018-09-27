21e10fbcf2ed28afbd6f5590581eafa1e06f3df3
kkwokwai22 <kkwokwai22@gmail.com>
2018-09-27 14:05:01 -0700

    set up reducer and store

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import store from "./store";
 
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
@@ -11,8 +11,6 @@ import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 
 import "./App.css";
-
-const store = createStore(() => [], {}, applyMiddleware());
 
 class App extends Component {
   render() {
